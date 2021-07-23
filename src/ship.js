974ccf903bfe2955d67cb32eb5d13b57f2b20a9c
reinimax <reinhold.schachner@hotmail.com>
2021-07-23 11:07:49 +0200

    create ship factory

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,55 @@
-const Ship = length => {
-  return { length };
+const Ship = coords => {
+  let position = setPosition(coords);
+  let length = position.length;
+  let sunk = false;
+
+  function setPosition(coords) {
+    let position = [];
+    let startX = Math.min(coords[0][0], coords[1][0]);
+    let startY = Math.min(coords[0][1], coords[1][1]);
+    let endX = Math.max(coords[0][0], coords[1][0]);
+    let endY = Math.max(coords[0][1], coords[1][1]);
+
+    if (startX > endX || endX > startX) {
+      let start = Math.min(startX, endX);
+      let end = Math.max(startX, endX);
+      for (let i = start; i <= end; i++) {
+        position.push({
+          x: i,
+          y: startY,
+          hit: false
+        });
+      }
+    }
+
+    if (startY > endY || endY > startY) {
+      let start = Math.min(startY, endY);
+      let end = Math.max(startY, endY);
+      for (let i = start; i <= end; i++) {
+        position.push({
+          x: startX,
+          y: i,
+          hit: false
+        });
+      }
+    }
+    return position;
+  }
+
+  function hit(coords) {
+    position.forEach(pos => {
+      if (pos.x === coords[0] && pos.y === coords[1]) {
+        pos.hit = true;
+      }
+    });
+    this.sunk = isSunk();
+  }
+
+  function isSunk() {
+    return position.filter(pos => pos.hit === false).length <= 0;
+  }
+
+  return { position, length, sunk, hit };
 };
 
 module.exports = Ship;
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,5 +1,130 @@
 const Ship = require('./ship');
 
-test('works', () => {
-  expect(Ship(1).length).toBe(1);
+test('returns the correct position with two fields', () => {
+  expect(
+    Ship([
+      [1, 1],
+      [2, 1]
+    ]).position
+  ).toStrictEqual([
+    { x: 1, y: 1, hit: false },
+    { x: 2, y: 1, hit: false }
+  ]);
+});
+
+test('returns the correct position with three fields', () => {
+  expect(
+    Ship([
+      [2, 3],
+      [4, 3]
+    ]).position
+  ).toStrictEqual([
+    { x: 2, y: 3, hit: false },
+    { x: 3, y: 3, hit: false },
+    { x: 4, y: 3, hit: false }
+  ]);
+});
+
+test('returns the correct position vertically', () => {
+  expect(
+    Ship([
+      [2, 3],
+      [2, 5]
+    ]).position
+  ).toStrictEqual([
+    { x: 2, y: 3, hit: false },
+    { x: 2, y: 4, hit: false },
+    { x: 2, y: 5, hit: false }
+  ]);
+});
+
+test('works when the second pair of coordinates is less, horizontally', () => {
+  expect(
+    Ship([
+      [2, 2],
+      [1, 2]
+    ]).position
+  ).toStrictEqual([
+    { x: 1, y: 2, hit: false },
+    { x: 2, y: 2, hit: false }
+  ]);
+});
+
+test('works when the second pair of coordinates is less, vertically', () => {
+  expect(
+    Ship([
+      [2, 3],
+      [2, 2]
+    ]).position
+  ).toStrictEqual([
+    { x: 2, y: 2, hit: false },
+    { x: 2, y: 3, hit: false }
+  ]);
+});
+
+test('has the correct length vertically', () => {
+  expect(
+    Ship([
+      [2, 3],
+      [2, 2]
+    ]).length
+  ).toBe(2);
+});
+
+test('has the correct length horizontally', () => {
+  expect(
+    Ship([
+      [2, 2],
+      [4, 2]
+    ]).length
+  ).toBe(3);
+});
+
+test('hit function hits the ship', () => {
+  let ship = Ship([
+    [2, 2],
+    [4, 2]
+  ]);
+  ship.hit([3, 2]);
+  expect(ship.position).toStrictEqual([
+    { x: 2, y: 2, hit: false },
+    { x: 3, y: 2, hit: true },
+    { x: 4, y: 2, hit: false }
+  ]);
+});
+
+test('hit function does not always hit, x coordinate', () => {
+  let ship = Ship([
+    [2, 2],
+    [4, 2]
+  ]);
+  ship.hit([5, 2]);
+  expect(ship.position).toStrictEqual([
+    { x: 2, y: 2, hit: false },
+    { x: 3, y: 2, hit: false },
+    { x: 4, y: 2, hit: false }
+  ]);
+});
+
+test('hit function does not always hit, y coordinate', () => {
+  let ship = Ship([
+    [2, 2],
+    [4, 2]
+  ]);
+  ship.hit([2, 3]);
+  expect(ship.position).toStrictEqual([
+    { x: 2, y: 2, hit: false },
+    { x: 3, y: 2, hit: false },
+    { x: 4, y: 2, hit: false }
+  ]);
+});
+
+test('ship is sunk correctly', () => {
+  let ship = Ship([
+    [1, 2],
+    [1, 3]
+  ]);
+  ship.hit([1, 2]);
+  ship.hit([1, 3]);
+  expect(ship.sunk).toBe(true);
 });
